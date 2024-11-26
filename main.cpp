d13d20e33e3fc96d218633b1740e064d37a1fcb6
Danielosw <65432065+danielosw@users.noreply.github.com>
2024-11-26 14:56:27 -0600

    add detection of numbers

--- main.cpp
+++ main.cpp
@@ -1,36 +1,57 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <string>
 using namespace std;
-double extractNumeric(const string& str){
-	double store = 0;
-	bool isDecimal = false;
-	for (int i = 0; i < str.length(); i++) {
-		switch (str[i]) {
-			case ""
-			default:
-				return -999999.99;	
-				break;
-		}
-	}
-	return -999999.99;
+double extractNumeric(const string &str) {
+  double store = 0;
+  bool isDecimal = false;
+  for (int i = 0; i < str.length(); i++) {
+    switch (str[i]) {
+    case '1':
+      return 1;
+      break;
+    case '2':
+      return 2;
+      break;
+    case '3':
+      return 3;
+      break;
+	case '4':
+	  return 4;
+	  break;
+	case '5':
+	  return 5;
+	  break;
+	case '6':
+	  return 6;
+	  break;
+	case '7':
+	  return 7;
+	  break;
+	case '8':
+	  return 8;
+	  break;
+	case '9':
+	  return 9;
+    }
+  }
+  return -999999.99;
 };
 
-int main(){
-	string input;
-	while (true){
-		cout << "Enter a string (or 'END' to quit): ";
-		cin >> input;
-		if(input == "END"){
-			break;
-		}
-		double number = extractNumeric(input);
-		if (number != -999999.99){
-			cout << "The input is: " << fixed << setprecision(4)
-				<< number << endl;
-		} else {
-			cout << "The input is invalid" << endl;
-		}
-	}
-	return 0;
+int main() {
+  string input;
+  while (true) {
+    cout << "Enter a string (or 'END' to quit): ";
+    cin >> input;
+    if (input == "END") {
+      break;
+    }
+    double number = extractNumeric(input);
+    if (number != -999999.99) {
+      cout << "The input is: " << fixed << setprecision(4) << number << endl;
+    } else {
+      cout << "The input is invalid" << endl;
+    }
+  }
+  return 0;
 }
